762e4ecddcdc5d5beffde8e71c7ba46e0ec5d8fa
Jason <jasonygodfrey@gmail.com>
2023-08-03 03:37:07 -0700

    ui animation benchmark

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
   navLinks.forEach((link) => {
       const href = link.getAttribute('href');
-      // Check if href matches the pathname exactly or when pathname has a trailing slash
       if (href === currentPathname || href + '/' === currentPathname) {
           link.classList.add('active-link');
       }
@@ -14,63 +13,74 @@ document.addEventListener('DOMContentLoaded', (event) => {
 const { GetRandom } = Phaser.Utils.Array;
 
 function preload() {
-this.load.crossOrigin = "anonymous";
-this.load.setBaseURL("https://labs.phaser.io");
-this.load.image("space", "assets/skies/space.jpg");
-this.load.image("spark", "assets/particles/blue.png");
+  this.load.crossOrigin = "anonymous";
+ // this.load.setBaseURL("https://labs.phaser.io");
+  this.load.image("space", "assets/space.jpg");
+  this.load.image("spark", "assets/blue.png");
+  this.load.spritesheet('uiSprite', 'assets/ui1spritesheet.png', { frameWidth: 224, frameHeight: 101 });
 }
 
 function create() {
-this.cameras.main.centerOn(0, 0);
+  this.cameras.main.centerOn(0, 0);
 
-const emitCircle = new Phaser.Geom.Circle(0, 0, 350);
-const moveToCircle = new Phaser.Geom.Circle(0, 0, 200);
-const moveToPoints = moveToCircle.getPoints(600);
+  const emitCircle = new Phaser.Geom.Circle(0, 0, 350);
+  const moveToCircle = new Phaser.Geom.Circle(0, 0, 200);
+  const moveToPoints = moveToCircle.getPoints(600);
 
-let moveToPoint = moveToPoints[0];
+  let moveToPoint = moveToPoints[0];
 
-this.add.image(0, 0, "space");
+  this.add.image(0, 0, "space");
 
-this.add.particles("spark").createEmitter({
-  alpha: { start: 0, end: 0.2, ease: "Cubic.easeInOut" },
-  blendMode: "ADD",
-  emitCallback: () => { moveToPoint = GetRandom(moveToPoints); },
-  emitZone: { source: emitCircle },
-  frequency: 1000 / 60,
-  lifespan: { min: 1000, max: 5000 },
-  moveToX: () => moveToPoint.x,
-  moveToY: () => moveToPoint.y,
-  quantity: 10,
-  scale: { start: 1, end: 0, ease: "Cubic.easeIn" }
-});
+  this.add.particles("spark").createEmitter({
+    alpha: { start: 0, end: 0.2, ease: "Cubic.easeInOut" },
+    blendMode: "ADD",
+    emitCallback: () => { moveToPoint = GetRandom(moveToPoints); },
+    emitZone: { source: emitCircle },
+    frequency: 1000 / 60,
+    lifespan: { min: 1000, max: 5000 },
+    moveToX: () => moveToPoint.x,
+    moveToY: () => moveToPoint.y,
+    quantity: 10,
+    scale: { start: 1, end: 0, ease: "Cubic.easeIn" }
+  });
+
+  const { gameWidth, gameHeight } = this.game.config;
+
+  let startGameText = this.add.text(gameWidth, 0, 'Start Game', { fontSize: '32px', fill: '#C71585' }).setOrigin(0.5, 4);
+  const sprite = this.add.sprite(0, 0, 'uiSprite').setOrigin(0.5, 0.5).setDepth(1000);
+
+  this.anims.create({
+    key: 'uiAnimate',
+    frames: this.anims.generateFrameNumbers('uiSprite', { start: 0, end: 124 })
+,
+    frameRate: 10,
+    repeat: -1
+  });
+
+  sprite.anims.play('uiAnimate');
 }
 
-// Calculate the window's aspect ratio
 const windowAspectRatio = window.innerWidth / window.innerHeight;
-
-// The desired aspect ratio for the game
 const gameAspectRatio = 3 / 2;
 
 let gameWidth, gameHeight;
 
 if (windowAspectRatio > gameAspectRatio) {
-  // If the window's aspect ratio is greater than the game's, then the height will be full,
-  // and the width will be calculated based on the game's aspect ratio.
   gameHeight = window.innerHeight;
   gameWidth = gameHeight * gameAspectRatio;
 } else {
-  // If the window's aspect ratio is less than the game's, then the width will be full,
-  // and the height will be calculated based on the game's aspect ratio.
   gameWidth = window.innerWidth;
   gameHeight = gameWidth / gameAspectRatio;
 }
 
 new Phaser.Game({
   type: Phaser.WEBGL,
-  parent: 'game-container',  // changed from 'game' to 'game-container'
-  width: gameWidth,  // adjusted width
-  height: gameHeight,  // adjusted height
+  parent: 'game-container',
+  width: gameWidth,
+  height: gameHeight,
   scene: { preload, create },
   audio: { noAudio: true },
-  input: { mouse: false, touch: false, keyboard: false }
+  input: { mouse: false, touch: false, keyboard: false },
+  gameWidth,
+  gameHeight
 });
